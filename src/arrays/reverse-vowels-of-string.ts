7e4e8f96dc144dd41a1bd87f9bf259bb355d4a5f
Lê Thanh Hiếu <hieulethanh243@gmail.com>
2025-10-08 21:07:21 +0700

    feat: reverse vowels of string

--- src/arrays/reverse-vowels-of-string.ts
+++ src/arrays/reverse-vowels-of-string.ts
@@ -1,6 +1,23 @@
 function reverseVowels(s: string): string {
-  const vowelsList = ["s", "e", "i", "o", "u", "S", "E", "I", "O", "U"];
+  const vowelsList = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
   const vowelsTemp = new Map<number, string>([]);
+  let temp = [];
+  let sStringToArr = s.split("");
 
-  return "";
+  for (let i = 0; i < s.length; i++) {
+    if (vowelsList.includes(s[i]!)) {
+      vowelsTemp.set(i, s[i]!);
+      temp.push(s[i]);
+    }
+  }
+  let count = temp.length - 1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (vowelsTemp.has(i)) {
+      sStringToArr[i] = temp[count] ?? "";
+      count--;
+    }
+  }
+
+  return sStringToArr.join("");
 }
--- src/two_pointers/move-zeroes.ts
+++ src/two_pointers/move-zeroes.ts
@@ -1,10 +1,9 @@
 function moveZeroes(nums: any[]): number[] {
-  let lastNonZeroIndex = 0; // 🔹 ban đầu vị trí đầu tiên để đặt số khác 0
+  let lastNonZeroIndex = 0;
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      // nếu là số khác 0, ta đổi chỗ nó về vị trí lastNonZeroIndex
       [nums[lastNonZeroIndex], nums[i]] = [nums[i], nums[lastNonZeroIndex]];
-      lastNonZeroIndex++; // 🔹 di chuyển sang vị trí tiếp theo
+      lastNonZeroIndex++;
     }
   }
   return nums;
